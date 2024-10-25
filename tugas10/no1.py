68d5600ccf0824abb1148794c567fe63c338874e
ardiansyah <47409108+ardian1996@users.noreply.github.com>
2024-10-25 18:25:11 +0700

    Update no1.py

--- tugas10/no1.py
+++ tugas10/no1.py
@@ -1,1 +1,37 @@
-mas mau nanya. github kalo udah di konekin ke ubuntu emang ga bisa upload lagi lewat via web github yahh. ga bisa upload soal nya via browser
+# Soal 1: Hello World to Python
+print("Hello World Python")
+
+# Soal 2: Aritmatika di Python
+# Pertambahan
+print(5 + 3)
+
+# Perkurangan
+print(10 - 2)
+
+# Perkalian
+print(4 * 3)
+
+# Pembagian
+print(16 / 4)
+
+# Soal 3: Assign Variable dan Tipe Data Integer, Float
+a = 10
+b = 3
+
+# Pembagian hasil integer
+int_division = a // b
+print(int_division)
+
+# Pembagian hasil float
+float_division = a / b
+print(float_division)
+
+# Soal 4: String Operation
+firstname = "Muhammad"
+lastname = "Ardiansyah"
+print(f"Hello sanbercode, saya {firstname} {lastname}! saya siap belajar python backend development.")
+
+# Soal 5: Tipe Data
+usia = 28
+kata = 'usiaku: '
+print(kata + str(usia) + ' tahun')
